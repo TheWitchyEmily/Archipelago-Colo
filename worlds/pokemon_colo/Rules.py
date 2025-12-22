2a6b63f365f05e4603c75081df9af19e06aec211
Zach <18561222+zellman01@users.noreply.github.com>
2025-12-22 00:59:15 -0500

    Fix server crash

--- worlds/pokemon_colo/Rules.py
+++ worlds/pokemon_colo/Rules.py
@@ -395,7 +395,7 @@ class ColosseumRules:
     def set_rules(self) -> None:
         multiworld = self.world.multiworld
         # Set victory based on goal
-        multiworld.get_location(Locations.Trainers.evice, self.player).place_locked_item(ColosseumItem("Goal", ItemClassification.progression, None, self.player))
+        multiworld.get_location(Locations.Trainers.evice, self.player).place_locked_item(ColosseumItem("Goal", ItemClassification.progression, 0000, self.player))
         multiworld.completion_condition[self.player] = lambda state: state.has("Goal", self.player)
         for region in multiworld.get_regions(self.player):
             if region.name in self.region_rules:
